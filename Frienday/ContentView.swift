9c6c6ee9840fbe6d390104b43a4c84084648277c
sotawakisaka29 <sota.wakisaka29@gmail.com>
2026-07-22 17:13:30 +0900

    Initial Commit

---
+++ Frienday/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Frienday
+//
+//  Created by 脇坂颯大 on 22/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Frienday/FriendayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendayApp.swift
+//  Frienday
+//
+//  Created by 脇坂颯大 on 22/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct FriendayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
